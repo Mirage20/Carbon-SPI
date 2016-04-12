7eea026be577381f57fea5254563a50b31238293
Mirage Abeysekara <mirage.abeysekara@gmail.com>
2016-04-12 16:26:32 +0530

    improved code quality of the test class

--- carbon-spi-hook/src/test/java/org/wso2/osgi/spi/processor/test/ConsumerProcessorTest.java
+++ carbon-spi-hook/src/test/java/org/wso2/osgi/spi/processor/test/ConsumerProcessorTest.java
@@ -17,14 +17,18 @@ public class ConsumerProcessorTest {
 
 
     @Test
-    public void setup() throws IOException {
+    public void testClassWeaving() throws IOException {
+
         ConsumerProcessor consumerProcessor = new ConsumerProcessor();
         String buildDir = System.getProperty("project.build.directory");
+
         Path mockClass = Paths.get(buildDir, "test-classes", "org", "wso2", "osgi", "spi", "processor",
                 "test", "mock", "MockConsumerClass.class");
+
         byte[] classBytes = Files.readAllBytes(mockClass);
         MockWovenClass mockWovenClass = new MockWovenClass(classBytes, MockConsumerClass.class.getName());
         consumerProcessor.weave(mockWovenClass);
+
         Assert.assertTrue(classBytes.length < mockWovenClass.getBytes().length, "Check if the mock class is woven.");
         Assert.assertTrue(mockWovenClass.getDynamicImports().contains(Constants.DYNAMIC_INJECT_PACKAGE_NAME),
                 "Check whether the dynamic package import is added");
