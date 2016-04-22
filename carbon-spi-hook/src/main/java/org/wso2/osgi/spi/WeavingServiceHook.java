28ef895ae8f9b968a319f65288da7cb3c74d9258
Miraj Abeysekara <miraj@wso2.com>
2016-04-22 12:27:59 +0530

    used carbon server lisnter notifications to register the WeavingHook service

--- carbon-spi-hook/src/main/java/org/wso2/osgi/spi/WeavingServiceHook.java
+++ carbon-spi-hook/src/main/java/org/wso2/osgi/spi/WeavingServiceHook.java
@@ -1,18 +1,25 @@
 package org.wso2.osgi.spi;
 
-import org.osgi.framework.BundleContext;
 import org.osgi.framework.hooks.weaving.WeavingHook;
-import org.wso2.carbon.launcher.FrameworkStartupHook;
+import org.wso2.carbon.launcher.CarbonServerEvent;
+import org.wso2.carbon.launcher.CarbonServerListener;
 import org.wso2.osgi.spi.processor.ConsumerProcessor;
 
 /**
  * Class for registering the weaving hook service to the OSGi framework via the system bundle.
  */
-public class WeavingServiceHook implements FrameworkStartupHook {
-    @Override
-    public void systemBundleStarted(BundleContext systemBundleContext) {
-        ConsumerProcessor consumerProcessor = new ConsumerProcessor();
-        systemBundleContext.registerService(WeavingHook.class, consumerProcessor, null);
+public class WeavingServiceHook implements CarbonServerListener {
 
+    /**
+     * Receives notification of a CarbonServerEvent.
+     *
+     * @param event CarbonServerEvent
+     */
+    @Override
+    public void notify(CarbonServerEvent event) {
+        if (event.getType() == CarbonServerEvent.BEFORE_LOADING_INITIAL_BUNDLES) {
+            ConsumerProcessor consumerProcessor = new ConsumerProcessor();
+            event.getSystemBundleContext().registerService(WeavingHook.class, consumerProcessor, null);
+        }
     }
 }
